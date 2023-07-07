d7c9c99dd02bbb94a7f9b26bc261e912392e0775
gaurav876409 <gaurav876409@gmail.com>
2023-07-07 17:21:24 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const fs = require("fs/promises");
 const fileName = "myfile.txt";
+fileContent = ",An online learning platform.";
 const updateFile = async (fileName, fileContent) => {
   // write code here
   // dont change function name
-  
+  fs.appendFile(fileName, fileContent, (error) => {
+    console.log(error)
+  })
 };
-
+updateFile(fileName, fileContent);
 module.exports = updateFile;
